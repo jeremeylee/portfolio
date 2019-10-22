75f57a93550bafdfd5b57562a43e9ab1c22f8bc1
Jeremey Lee <leejeremey@gmail.com>
2019-10-21 17:43:30 -0700

    Created projects page

---
+++ src/pages/projects.js
@@ -0,0 +1,12 @@
+import React from "React"
+import Layout from "../components/layout"
+
+const ProjectsPage = () => {
+  return (
+    <Layout>
+      <div>Test</div>
+    </Layout>
+  )
+}
+
+export default ProjectsPage
