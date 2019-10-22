a35c9012305f81be925378cfb9063b35a91821c5
Jeremey Lee <leejeremey@gmail.com>
2019-10-21 20:27:49 -0700

    Created test divs for testing smooth scrolling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -14,6 +14,33 @@ const IndexPage = () => (
       CSS
     </p>
     <p>Want more from me? Ask and you shall receive</p>
+    <Link to="/#test">test</Link>
+    <Link to="/#test2">test2</Link>
+    <Link to="/#test3">test3</Link>
+    <div
+      id="test"
+      style={{
+        width: "500px",
+        height: "600px",
+        background: "red",
+      }}
+    ></div>
+    <div
+      id="test2"
+      style={{
+        width: "500px",
+        height: "600px",
+        background: "green",
+      }}
+    ></div>
+    <div
+      id="test3"
+      style={{
+        width: "500px",
+        height: "600px",
+        background: "yellow",
+      }}
+    ></div>
   </Layout>
 )
 
