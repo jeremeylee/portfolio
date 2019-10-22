fe54c0508d35828869e33336e03f5ab9e79994b4
Jeremey Lee <leejeremey@gmail.com>
2019-10-21 22:21:46 -0700

    Added styling

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,13 +1,21 @@
 import React from "React"
 import Layout from "../components/layout"
 import JSONDATA from "../content/portfolio-content.json"
+import Project from "../components/project"
 
 const ProjectsPage = () => {
   return (
     <Layout>
       <div>
-        {JSONDATA.content.map(data => (
-          <p>{data.title}</p>
+        {JSONDATA.content.map((data, index) => (
+          <Project
+            key={index}
+            title={data.title}
+            description={data.description}
+            tech={data.tech}
+            live={data.live}
+            source={data.source}
+          />
         ))}
       </div>
     </Layout>
