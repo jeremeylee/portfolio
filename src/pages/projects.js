554f0e67debdfe796707d2ce17d8908b79b85c7a
Jeremey Lee <leejeremey@gmail.com>
2019-10-21 18:54:44 -0700

    Tested out implementation of JSON data

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,10 +1,15 @@
 import React from "React"
 import Layout from "../components/layout"
+import JSONDATA from "../content/portfolio-content.json"
 
 const ProjectsPage = () => {
   return (
     <Layout>
-      <div>Test</div>
+      <div>
+        {JSONDATA.content.map(data => (
+          <p>{data.title}</p>
+        ))}
+      </div>
     </Layout>
   )
 }
