0b24a9d19bc930f4badeb3466da4e88c258b3186
Jeremey Lee <leejeremey@gmail.com>
2019-10-21 22:20:32 -0700

    Changed website background

--- src/components/project.js
+++ src/components/project.js
@@ -1,7 +1,23 @@
 import React from "react"
+import "./project.css"
 
 const Project = props => {
-  return <div>test</div>
+  const techs = props.tech.split(",")
+  return (
+    <div class="project-card">
+      <div class="content-background">
+        <div class="project-content">
+          <h3>{props.title}</h3>
+          <p>{props.description}</p>
+          {techs.map(tech => (
+            <p>{tech}</p>
+          ))}
+          <p>{props.live}</p>
+          <p>{props.source}</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Project
