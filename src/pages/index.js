5dc1bd8c9d8da5fe9f51b8c18da280309c12546f
Jeremey Lee <leejeremey@gmail.com>
2019-10-28 15:31:19 -0700

    Added typing animation for header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import React from "react"
-import { Link } from "gatsby"
-import AniLink from "gatsby-plugin-transition-link/AniLink"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
+import Typist from "react-typist"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hey, I'm Jeremey Lee</h1>
+
+    <h1>
+      <Typist>Hey, I'm Jeremey Lee</Typist>
+    </h1>
+
     <hr />
     <p>I'm a software engineer who specializes in full-stack development</p>
   </Layout>
